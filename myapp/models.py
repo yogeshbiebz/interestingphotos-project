dae80b75206a771a06cdf704407883e344cf183f
Yogesh Kumar <yogesh.biebz@gmail.com>
2017-07-14 22:30:42 +0530

    User model created, one user added to database.

--- myapp/models.py
+++ myapp/models.py
@@ -4,3 +4,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    name = models.CharField(max_length=30)
+    contact = models.IntegerField(max_length=13)
+    age = models.IntegerField(max_length=3)
+    created_on = models.DateTimeField(auto_now_add=True)
