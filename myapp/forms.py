fb798d7dfb022ae743bb9bad84c67fb1f9d59c78
Yogesh Kumar <yogesh.biebz@gmail.com>
2017-07-22 19:12:40 +0530

    Signup Created, Login under construction

--- myapp/forms.py
+++ myapp/forms.py
@@ -2,8 +2,12 @@ from django import forms
 from models import User
 
 
-class SignUpForm(forms.ModelForm):
+class SignUp(forms.ModelForm):
     class Meta:
         model = User
         fields=['email','username','name','password']
 
+class LogIn(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'password']
--- myapp/views.py
+++ myapp/views.py
@@ -2,15 +2,24 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-from forms import SingUpForm
-
+from forms import SignUp
+from django.contrib.auth.hashers import make_password
 
 # Create your views here.
 
 
 def home_view(request):
     if request.method == "POST":
-        form = SignUpForm(request.POST)
+        form = SignUp(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = User(name=name, password=make_password(password), email=email, username=username)
+            user.save()
+            render(request, 'success.html')
+
     elif request.method == "GET":
-        form = SignUpForm()
+        form = SignUp()
     return render(request, 'home.html')
